769c8cffb500c964451f4d0cc2912a31d39dc83b
José Pereira <jose.as.pereira@gmail.com>
2025-11-27 18:13:57 +0000

    twenty_third commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from '@/App.vue';
 import '@/assets/scss/styles.scss'; // <-- Import the global styles
 import i18n, { loadLocaleMessages } from '@/i18n/i18n.js';
 
 const app = createApp(App);
+const pinia = createPinia();
+
 app.use(i18n);
 // Define the initial locale - get it from i18n
 const initialLocale = i18n.global.locale.value; 
 // Await the initial loading function
 await loadLocaleMessages(initialLocale);
+app.use(pinia);
 app.mount('#app');
